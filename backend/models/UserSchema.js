c3f21cd0584eb52c9d81545306b6bc75ef9daa72
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-03-17 22:20:47 +0200

    adding isverified

--- backend/models/UserSchema.js
+++ backend/models/UserSchema.js
@@ -31,6 +31,10 @@ const UserSchema = new mongoose.Schema({
   resetToken: {
     type: String,
   },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const User = mongoose.model("User", UserSchema);
