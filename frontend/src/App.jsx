73a4e72837870572b45d409747a1748ecb1315fd
elgharbi <elgharbiay@gmail.com>
2024-03-19 17:35:29 +0000

    init front end with raectjs and shadcn

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>hello ahmed and mahmoud</h1>
+      <Button>Welcome</Button>
+    </>
+  );
+}
+
+export default App;
