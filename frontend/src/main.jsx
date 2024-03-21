052fd53a557dd1218eb94508298db19cd1cb7537
elgharbi <elgharbiay@gmail.com>
2024-03-21 01:48:23 +0000

    Refactor UI components and add new features

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,21 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import NavBar from "./components/NavBar/NavBar.jsx";
 import { UserProvider } from "./ContextAPI/UserContext.jsx";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Footer from "./components/Footer/Footer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<ToastContainer />
-			<UserProvider>
-				<NavBar />
-				<App />
-				<Footer />
-			</UserProvider>
-		</BrowserRouter>
-	</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
