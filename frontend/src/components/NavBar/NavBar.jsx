1865e6424f48b95c1325bb355dd3ab05b24eb61c
MahmoudEasa <mu01011422865@gmail.com>
2024-03-21 02:51:46 +0200

    Change NavBar Menu

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -73,136 +73,118 @@ const NavBar = () => {
 									</div>
 								</div>
 							</div>
-							<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-								<button
-									type="button"
-									className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-								>
-									<span className="absolute -inset-1.5" />
-									<span className="sr-only">
-										View notifications
-									</span>
-									<BellIcon
-										className="h-6 w-6"
-										aria-hidden="true"
-									/>
-								</button>
-
-								{/* Profile dropdown */}
-								<Menu as="div" className="relative ml-3">
-									<div>
-										<Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-											<span className="absolute -inset-1.5" />
-											<span className="sr-only">
-												Open user menu
-											</span>
-											<img
-												className="h-8 w-8 rounded-full"
-												src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-												alt=""
-											/>
-										</Menu.Button>
-									</div>
-									<Transition
-										as={Fragment}
-										enter="transition ease-out duration-100"
-										enterFrom="transform opacity-0 scale-95"
-										enterTo="transform opacity-100 scale-100"
-										leave="transition ease-in duration-75"
-										leaveFrom="transform opacity-100 scale-100"
-										leaveTo="transform opacity-0 scale-95"
+							{user ? (
+								<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+									<button
+										type="button"
+										className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
 									>
-										<Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-											{user ? (
-												<>
-													<Menu.Item>
-														{({ active }) => (
-															<NavLink
-																to="/profile"
-																className={classNames(
-																	active
-																		? "bg-gray-100"
-																		: "",
-																	"block px-4 py-2 text-sm text-gray-700"
-																)}
-															>
-																Your Profile
-															</NavLink>
-														)}
-													</Menu.Item>
-													<Menu.Item>
-														{({ active }) => (
-															<NavLink
-																to="/settings"
-																className={classNames(
-																	active
-																		? "bg-gray-100"
-																		: "",
-																	"block px-4 py-2 text-sm text-gray-700"
-																)}
-															>
-																Settings
-															</NavLink>
-														)}
-													</Menu.Item>
-													<Menu.Item>
-														{({ active }) => (
-															<NavLink
-																onClick={() =>
-																	signOut(
-																		cookieValue
-																	)
-																}
-																className={classNames(
-																	active
-																		? "bg-gray-100"
-																		: "",
-																	"block px-4 py-2 text-sm text-gray-700"
-																)}
-															>
-																Sign out
-															</NavLink>
-														)}
-													</Menu.Item>
-												</>
-											) : (
-												<>
-													<Menu.Item>
-														{({ active }) => (
-															<NavLink
-																to="/signup"
-																className={classNames(
-																	active
-																		? "bg-gray-100"
-																		: "",
-																	"block px-4 py-2 text-sm text-gray-700"
-																)}
-															>
-																Sign UP
-															</NavLink>
-														)}
-													</Menu.Item>
-													<Menu.Item>
-														{({ active }) => (
-															<NavLink
-																to="/login"
-																className={classNames(
-																	active
-																		? "bg-gray-100"
-																		: "",
-																	"block px-4 py-2 text-sm text-gray-700"
-																)}
-															>
-																Login
-															</NavLink>
-														)}
-													</Menu.Item>
-												</>
-											)}
-										</Menu.Items>
-									</Transition>
-								</Menu>
-							</div>
+										<span className="absolute -inset-1.5" />
+										<span className="sr-only">
+											View notifications
+										</span>
+										<BellIcon
+											className="h-6 w-6"
+											aria-hidden="true"
+										/>
+									</button>
+
+									{/* Profile dropdown */}
+									<Menu as="div" className="relative ml-3">
+										<div>
+											<Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+												<span className="absolute -inset-1.5" />
+												<span className="sr-only">
+													Open user menu
+												</span>
+												<img
+													className="h-8 w-8 rounded-full"
+													src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+													alt=""
+												/>
+											</Menu.Button>
+										</div>
+										<Transition
+											as={Fragment}
+											enter="transition ease-out duration-100"
+											enterFrom="transform opacity-0 scale-95"
+											enterTo="transform opacity-100 scale-100"
+											leave="transition ease-in duration-75"
+											leaveFrom="transform opacity-100 scale-100"
+											leaveTo="transform opacity-0 scale-95"
+										>
+											<Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+												<Menu.Item>
+													{({ active }) => (
+														<NavLink
+															to="/profile"
+															className={classNames(
+																active
+																	? "bg-gray-100"
+																	: "",
+																"block px-4 py-2 text-sm text-gray-700"
+															)}
+														>
+															Your Profile
+														</NavLink>
+													)}
+												</Menu.Item>
+												<Menu.Item>
+													{({ active }) => (
+														<NavLink
+															to="/settings"
+															className={classNames(
+																active
+																	? "bg-gray-100"
+																	: "",
+																"block px-4 py-2 text-sm text-gray-700"
+															)}
+														>
+															Settings
+														</NavLink>
+													)}
+												</Menu.Item>
+												<Menu.Item>
+													{({ active }) => (
+														<NavLink
+															onClick={() =>
+																signOut(
+																	cookieValue
+																)
+															}
+															className={classNames(
+																active
+																	? "bg-gray-100"
+																	: "",
+																"block px-4 py-2 text-sm text-gray-700"
+															)}
+														>
+															Sign out
+														</NavLink>
+													)}
+												</Menu.Item>
+											</Menu.Items>
+										</Transition>
+									</Menu>
+								</div>
+							) : (
+								<>
+									<NavLink
+										to="/login"
+										className="block px-4 py-2 text-sm text-gray-300
+													hover:text-white"
+									>
+										Login
+									</NavLink>
+									<NavLink
+										to="/signup"
+										className="block px-4 py-2 text-sm text-gray-300
+													hover:text-white"
+									>
+										Sign UP
+									</NavLink>
+								</>
+							)}
 						</div>
 					</div>
 
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -30,7 +30,6 @@ const people = [
 		imageUrl:
 			"https://firebasestorage.googleapis.com/v0/b/portfolioecommerce-f1ee6.appspot.com/o/images%2Fa07fb189-7493-424d-b073-8216b4e293dd?alt=media&token=8f057f1b-c8a6-440d-ad22-3f3fc2e3b416",
 	},
-	// More people...
 ];
 
 const url = "http://164.92.225.123:5000";
