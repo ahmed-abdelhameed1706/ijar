147664d69960d56202498123c6dd689b5bb59e4f
elgharbi <elgharbiay@gmail.com>
2024-03-25 06:15:44 +0000

    add a booking list page to dashboard

--- frontend/src/pages/dashboard/bookingList/BookingList.jsx
+++ frontend/src/pages/dashboard/bookingList/BookingList.jsx
@@ -1,60 +1,106 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@radix-ui/react-label";
-import { Search } from "lucide-react";
 import { DataTable } from "../data-table";
 import { columns } from "../columns";
+import { useState } from "react";
 
 const data = [
   {
-    status: "Pending",
-    email: "",
-    amount: "",
+    status: "In Progress",
+    carName: "BMW",
+    carModel: "X5",
+    deliverylocation: "Lagos",
+    pickupLocation: "Abuja",
+    BookingDate: "12/12/2021",
+    total: "$200",
   },
   {
-    status: "Pending",
-    email: "",
-    amount: "",
+    status: "Canceled",
+    carName: "Toyota",
+    carModel: "Camry",
+    deliverylocation: "Lagos",
+    pickupLocation: "Abuja",
+    BookingDate: "12/12/2021",
+    total: "$500",
   },
   {
-    status: "Pending",
-    email: "",
-    amount: "",
+    status: "Completed",
+    carName: "Mercedes",
+    carModel: "Benz",
+    deliverylocation: "Lagos",
+    pickupLocation: "Abuja",
+    BookingDate: "12/12/2021",
+    total: "$200",
   },
   {
-    status: "Pending",
-    email: "",
-    amount: "",
+    status: "In Progress",
+    carName: "Toyota",
+    carModel: "Camry",
+    deliverylocation: "Lagos",
+    pickupLocation: "Abuja",
+    BookingDate: "12/12/2021",
+    total: "$300",
   },
 ];
 
 const BookingList = () => {
+  const [activeTab, setActiveTab] = useState("all");
+  const [dataTable, setDataTable] = useState(data);
+
+  const handleFilter = (status) => {
+    const filteredData = data.filter((item) => item.status === status);
+    setDataTable(filteredData);
+  };
+  const handleStatus = (status) => {
+    setActiveTab(status);
+    if (status === "all") {
+      setDataTable(data);
+    } else {
+      handleFilter(status);
+    }
+  };
+
   return (
     <div>
       <h1 className="text-xl font-bold mb-4">Dashboard</h1>
       <div className="p-4 flex gap-4 bg-white rounded-lg mb-6">
-        <Button variant="secondary">All Booking</Button>
-        <Button variant="outline">Upcoming</Button>
-        <Button variant="outline">Completed</Button>
-        <Button variant="outline">Canceled</Button>
-        <Button variant="outline">In Progress</Button>
+        <Button
+          onClick={() => handleStatus("all")}
+          variant={activeTab === "all" ? "secondary" : "outline"}
+        >
+          All Booking
+        </Button>
+        <Button
+          onClick={() => handleStatus("Upcoming")}
+          variant={activeTab === "Upcoming" ? "secondary" : "outline"}
+        >
+          Upcoming
+        </Button>
+        <Button
+          onClick={() => handleStatus("Completed")}
+          variant={activeTab === "Completed" ? "secondary" : "outline"}
+        >
+          Completed
+        </Button>
+        <Button
+          variant={activeTab === "Pending" ? "secondary" : "outline"}
+          onClick={() => handleStatus("Pending")}
+        >
+          Canceled
+        </Button>
+        <Button
+          variant={activeTab === "In Progress" ? "secondary" : "outline"}
+          onClick={() => handleStatus("In Progress")}
+        >
+          In Progress
+        </Button>
       </div>
       <div className="p-4 bg-white rounded-lg">
-        <div className="flex justify-between">
-          <h2 className="text-lg font-bold mb-8 mt-2">All Booking</h2>
-          <Label className="flex justify-center  items-center border border-gray-400 rounded-lg w-[300px] h-11">
-            <Input
-              className="border-none rounded-lg w-full outline-none ring-0 p-2 placeholder:text-base"
-              type="search"
-              placeholder="Search"
-            />
-            <div className="p-2">
-              <Search strokeWidth={2.25} />
-            </div>
-          </Label>
+        <div className="px-4 flex justify-between">
+          <h2 className="text-lg font-bold my-2">All Booking</h2>
         </div>
         <div className="p-4">
-          <DataTable columns={columns} data={data} />
+          <DataTable columns={columns} data={dataTable} />
         </div>
       </div>
     </div>
--- frontend/src/pages/dashboard/data-table.jsx
+++ frontend/src/pages/dashboard/data-table.jsx
@@ -1,9 +1,12 @@
 /* eslint-disable react/prop-types */
+"use client";
 import {
   flexRender,
   getCoreRowModel,
   useReactTable,
   getPaginationRowModel,
+  getSortedRowModel,
+  getFilteredRowModel,
 } from "@tanstack/react-table";
 
 import {
@@ -15,17 +18,94 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDownIcon, Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export function DataTable({ columns, data }) {
+  const [sorting, setSorting] = useState([]);
+  const [columnFilters, setColumnFilters] = useState();
+  const [columnVisibility, setColumnVisibility] = useState({});
+  const [rowSelection, setRowSelection] = useState({});
+
   const table = useReactTable({
     data,
     columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+    initialState: {
+      filters: [
+        {
+          id: "carName",
+          value: "",
+        },
+      ],
+    },
   });
 
+  const onChangeFilter = (id, value) => {
+    table.getColumn(id)?.setFilterValue(value);
+  };
   return (
     <div>
+      <div className="flex items-center py-4">
+        <Label className="flex justify-center  items-center border border-gray-300 rounded-lg w-[300px] h-11">
+          <Input
+            className="border-none rounded-lg w-full outline-none ring-0 p-2 placeholder:text-base"
+            value={table.getColumn("carName")?.getFilterValue() ?? ""}
+            placeholder="Search"
+            onChange={(e) => onChangeFilter("carName", e.target.value)}
+          />
+          <div className="p-2">
+            <Search strokeWidth={2.25} className="text-gray-300" />
+          </div>
+        </Label>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Columns <ChevronDownIcon className="ml-2 h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
       <div className="rounded-md border">
         <Table>
           <TableHeader>
@@ -33,7 +113,7 @@ export function DataTable({ columns, data }) {
               <TableRow key={headerGroup.id}>
                 {headerGroup.headers.map((header) => {
                   return (
-                    <TableHead key={header.id}>
+                    <TableHead key={header.id} className=" text-center">
                       {header.isPlaceholder
                         ? null
                         : flexRender(
@@ -54,7 +134,7 @@ export function DataTable({ columns, data }) {
                   data-state={row.getIsSelected() && "selected"}
                 >
                   {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
+                    <TableCell key={cell.id} className="text-center">
                       {flexRender(
                         cell.column.columnDef.cell,
                         cell.getContext()
