e6b24acd3f756273f8948ca200ab63554cd27848
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-03-29 02:43:50 +0200

    Add faker library for generating fake data

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,8 @@ import ticketRouter from "./routes/ticketRoutes";
 import { activityLogger } from "./utils/middlewares";
 import cookieParser from "cookie-parser";
 import path from "path";
+import Car from "./models/CarSchema";
+import { faker } from "@faker-js/faker";
 
 const port = 5000;
 const app = express();
@@ -50,6 +52,41 @@ mongoose.connect("mongodb://localhost:27017/Ijar", {
   // useUnifiedTopology: true,
 });
 
+const seedDatabase = async (numCars = 10) => {
+  try {
+    for (let i = 0; i < numCars; i++) {
+      const newCar = new Car({
+        brandName: faker.vehicle.manufacturer(),
+        model: faker.vehicle.model(),
+        year: faker.number.int({ min: 1950, max: 2024 }),
+        type: faker.vehicle.type(),
+        color: faker.vehicle.color(),
+        location: faker.location.city(),
+        fuel: faker.vehicle.fuel(),
+        maxSpeed: faker.number.int({ min: 100, max: 350 }),
+        price: faker.number.int({ min: 80, max: 1000 }),
+        licensePlateNumber: faker.datatype.string({
+          length: 7,
+          charset: "ABC1234567890",
+        }),
+        engineId: faker.datatype.string({
+          length: 15,
+          charset: "ABCDEF1234567890",
+          unique: true,
+        }),
+        description: faker.vehicle.vehicle(),
+        ownerId: new mongoose.Types.ObjectId(),
+      });
+
+      await newCar.save();
+      console.log(`Car ${i + 1} created successfully!`);
+    }
+    console.log(`Database populated with ${numCars} cars!`);
+  } catch (error) {
+    console.error("Error populating database:", error);
+  }
+};
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Event listeners for MongoDB connection
@@ -59,6 +96,7 @@ mongoose.connection.on("connected", () => {
     app.listen(port, () => {
       console.log(`Server connected to http://localhost:${port}`);
     });
+    seedDatabase(50);
   } catch (error) {
     console.log("Cannot connect to the server");
   }
