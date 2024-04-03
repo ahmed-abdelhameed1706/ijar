3da15a4f04d3087162fbdfb990d243ba0ee9a93d
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-04-03 02:49:24 +0200

    Refactor PayPal API integration

--- backend/controllers/payPal.js
+++ backend/controllers/payPal.js
@@ -1,146 +1,129 @@
-import axios from "axios";
-import e from "express";
+import fetch from "node-fetch";
 
 const base = "https://api-m.sandbox.paypal.com";
 
 const clientID = process.env.PAYPAL_ID;
 const clientSecret = process.env.PAYPAL_SECRET;
 
+const generateAccessToken = async () => {
+  try {
+    if (!clientID || !clientSecret) {
+      throw new Error("MISSING_API_CREDENTIALS");
+    }
+
+    const auth = Buffer.from(clientID + ":" + clientSecret).toString("base64");
+
+    const response = await fetch(`${base}/v1/oauth2/token`, {
+      method: "POST",
+
+      body: "grant_type=client_credentials",
+
+      headers: {
+        Authorization: `Basic ${auth}`,
+      },
+    });
+
+    const data = await response.json();
+
+    return data.access_token;
+  } catch (error) {
+    console.error("Failed to generate Access Token:", error.message);
+  }
+};
 export default class PayPal {
-  static async handleResponse(response) {
-    try {
-      const jsonResponse = response.data;
-
-      return {
-        jsonResponse,
-        httpStatusCode: response.status,
-      };
-    } catch (err) {
-      const errorMessage = err.response.data;
-
-      throw new Error(errorMessage);
-    }
-  }
-
-  static generateAccessToken = async () => {
-    try {
-      if (!clientID || !clientSecret) {
-        throw new Error("MISSING_API_CREDENTIALS");
-      }
-
-      const auth = Buffer.from(clientID + ":" + clientSecret).toString(
-        "base64"
-      );
-
-      const response = await axios.post(
-        `${base}/v1/oauth2/token`,
-        "grant_type=client_credentials",
-        {
-          headers: {
-            Authorization: `Basic ${auth}`,
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        }
-      );
-
-      const data = response.data;
-
-      return data.access_token;
-    } catch (error) {
-      console.error("Failed to generate Access Token:", error.message);
-    }
-  };
-
   static createOrder = async (cart) => {
-    try {
-      // use the cart information passed from the front-end to calculate the purchase unit details
-      console.log("----------------");
-      console.log("--------CREATE ORDER SERVICE--------");
-      console.log("----------------");
+    // use the cart information passed from the front-end to calculate the purchase unit details
 
-      console.log(
-        "shopping cart information passed from the frontend createOrder() callback:",
-        cart
-      );
+    console.log(
+      "shopping cart information passed from the frontend createOrder() callback:",
 
-      console.log("car Id is " + cart[0].car.id);
+      cart
+    );
 
-      const accessToken = await PayPal.generateAccessToken();
-      console.log("Access Token:", accessToken);
+    const accessToken = await generateAccessToken();
 
-      const url = `${base}/v2/checkout/orders`;
+    const url = `${base}/v2/checkout/orders`;
 
-      const payload = {
-        intent: "CAPTURE",
-        purchase_units: [
-          {
-            amount: {
-              currency_code: "USD",
-              value: cart[0].price,
-            },
+    const payload = {
+      intent: "CAPTURE",
+
+      purchase_units: [
+        {
+          amount: {
+            currency_code: "USD",
+
+            value: cart[0].price,
           },
-        ],
-      };
-
-      const response = await axios.post(url, payload, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
         },
-      });
+      ],
+    };
 
-      const orderData = PayPal.handleResponse(response);
-      console.log("Response from PayPal.js:", orderData);
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
 
-      return orderData;
-    } catch (error) {
-      console.error("Failed to create order from paypal.js:", error.message);
-    }
+        Authorization: `Bearer ${accessToken}`,
+
+        // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
+
+        // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
+
+        // "PayPal-Mock-Response": '{"mock_application_codes": "MISSING_REQUIRED_PARAMETER"}'
+
+        // "PayPal-Mock-Response": '{"mock_application_codes": "PERMISSION_DENIED"}'
+
+        // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
+      },
+
+      method: "POST",
+
+      body: JSON.stringify(payload),
+    });
+
+    return handleResponse(response);
   };
-
-  static async getApprovalLink(orderID) {
-    return `https://www.sandbox.paypal.com/checkoutnow?token=${orderID}`;
-  }
 
   static captureOrder = async (orderID) => {
-    console.log("----------------");
-    console.log("--------CAPTURE ORDER SERVICE--------");
-    console.log("----------------");
-    try {
-      const accessToken = await PayPal.generateAccessToken();
-      console.log("Access Token from capture:", accessToken);
+    const accessToken = await generateAccessToken();
 
-      const url = `${base}/v2/checkout/orders/${orderID}/capture`;
+    const url = `${base}/v2/checkout/orders/${orderID}/capture`;
 
-      console.log("url is " + url);
+    const response = await fetch(url, {
+      method: "POST",
 
-      const response = await axios.post(url, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
+      headers: {
+        "Content-Type": "application/json",
 
-      console.log("Response from captureOrder:", response.data);
-      console.log("HTTPResponse from captureOrder:", response.status);
+        Authorization: `Bearer ${accessToken}`,
 
-      const data = PayPal.handleResponse(response);
+        // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
 
-      return data;
-    } catch (error) {
-      if (error.response) {
-        console.error(
-          "Failed to capture order from paypal.js:",
-          error.response.data,
-          error.response.status
-        );
-      } else if (error.request) {
-        console.error("Failed to capture order from paypal.js:", error.request);
-      } else {
-        console.error("Failed to capture order from paypal.js:", error.message);
-      }
+        // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
 
-      throw error;
-    }
+        // "PayPal-Mock-Response": '{"mock_application_codes": "INSTRUMENT_DECLINED"}'
+
+        // "PayPal-Mock-Response": '{"mock_application_codes": "TRANSACTION_REFUSED"}'
+
+        // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
+      },
+    });
+
+    return handleResponse(response);
   };
+}
+
+async function handleResponse(response) {
+  try {
+    const jsonResponse = await response.json();
+
+    return {
+      jsonResponse,
+
+      httpStatusCode: response.status,
+    };
+  } catch (err) {
+    const errorMessage = await response.text();
+
+    throw new Error(errorMessage);
+  }
 }
