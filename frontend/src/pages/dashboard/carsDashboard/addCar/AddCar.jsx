b786d3390529e43b15dd90d3d33c47eea1ff64e6
alidrisy <a.alidrisy6@gmail.com>
2024-04-04 07:05:37 +0300

    fix add car form

--- frontend/src/pages/dashboard/carsDashboard/addCar/AddCar.jsx
+++ frontend/src/pages/dashboard/carsDashboard/addCar/AddCar.jsx
@@ -83,7 +83,7 @@ const AddCar = ({ setOpen, setCars, cars, car, isUpdate = false }) => {
     },
   });
 
-  async function onSubmit() {
+  async function onSubmit(values) {
     toast.promise(uploadImages(images), {
       pending: isUpdate ? "Updating Car..." : "Adding Car...",
       error: isUpdate ? "Failed to update car." : "Failed to add car.",
@@ -94,7 +94,7 @@ const AddCar = ({ setOpen, setCars, cars, car, isUpdate = false }) => {
       if (isUpdate) {
         const response = await axios.put(
           `/api/cars/${car.id}`,
-          JSON.stringify({ ...form.getValues(), images: imagesUrl }),
+          JSON.stringify({ ...values, images: imagesUrl }),
           {
             headers: {
               "Content-Type": "application/json",
@@ -109,7 +109,7 @@ const AddCar = ({ setOpen, setCars, cars, car, isUpdate = false }) => {
       }
       const response = await axios.post(
         "/api/cars",
-        JSON.stringify({ ...form.getValues(), images: imagesUrl }),
+        JSON.stringify({ ...values, images: imagesUrl }),
         {
           headers: {
             "Content-Type": "application/json",
@@ -323,6 +323,8 @@ const AddCar = ({ setOpen, setCars, cars, car, isUpdate = false }) => {
                     </FormItem>
                   )}
                 />
+                </div>
+                <div className="flex flex-col sm:flex-row gap-4">
                 <FormField
                   control={form.control}
                   name="color"
@@ -331,6 +333,19 @@ const AddCar = ({ setOpen, setCars, cars, car, isUpdate = false }) => {
                       <FormLabel>Color</FormLabel>
                       <FormControl>
                         <Input placeholder="Black" type="text" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="year"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Year</FormLabel>
+                      <FormControl>
+                        <Input placeholder="2023" type="number" {...field} />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
@@ -451,7 +466,7 @@ const AddCar = ({ setOpen, setCars, cars, car, isUpdate = false }) => {
                   </FormItem>
                 )}
               />
-              <Button className="w-full" type="submit" onClick={onSubmit}>
+              <Button className="w-full" type="submit">
                 {isUpdate ? "Update Car" : "Add Car"}
               </Button>
             </form>
